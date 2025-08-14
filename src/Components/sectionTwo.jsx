5d3c57a641673436ac6b469ab1cf7e1a6d912df7
Anas <anascabdi199@gmail.com>
2025-08-14 17:42:33 +0930

    sectionTwo

---
+++ src/Components/sectionTwo.jsx
@@ -0,0 +1,48 @@
+function SectionTwo(){
+    return <div>
+        <div className="flex py-10 mt-20 justify-center">
+            <div>
+                <div className="text-right relative text-[1.2em]">
+                <img className="absolute right-0" src="https://xtratheme.com/elementor/watch-shop/wp-content/uploads/sites/79/2021/10/dashboard.png" alt="" />
+                <h1 className="font-extrabold text-[1em] pt-16">Extraordinery Performance</h1>
+                <p className="w-80 ">A smartwatch is a wearable computer in the form of a watch</p>
+                </div>
+
+                <div className="text-right relative text-[1.2em]">
+                <img className="absolute right-0 pt-10" src="https://xtratheme.com/elementor/watch-shop/wp-content/uploads/sites/79/2021/10/batteryv.png" alt="" />
+                <h1 className="font-extrabold text-[1em] pt-20">Excellent battery life</h1>
+                <p className="w-64 ml-14 ">A smartwatch is a wearable computer in the form of a watch.</p>
+                </div>
+
+                <div className="text-right relative text-[1.2em]">
+                <img className="absolute right-0 pt-10" src="https://xtratheme.com/elementor/watch-shop/wp-content/uploads/sites/79/2021/10/link.png" alt="" />
+                <h1 className="font-extrabold text-[1em] pt-24">Connectable to android/iOS</h1>
+                <p className="w-80  ">A smartwatch is a wearable computer in the form of a watch.</p>
+                </div>
+            </div>
+            <div>
+                <img className="w-[500px] pt-10" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7goHgIj5jTIOZd_hwZksOfitgDdLvgDkqfqaUiCicak3DfmsY" alt="" />
+            </div>
+            <div>
+                <div className="text-left relative text-[1.2em]">
+                <img  src="https://xtratheme.com/elementor/watch-shop/wp-content/uploads/sites/79/2021/10/calendar.png" alt="" />
+                <h1 className="font-extrabold text-[1em] pt-5">Shows time & date</h1>
+                <p className="w-80 ">A smartwatch is a wearable computer in the form of a watch.</p>
+                </div>
+
+                <div className="text-left relative text-[1.2em]">
+                <img className="pt-10" src="https://xtratheme.com/elementor/watch-shop/wp-content/uploads/sites/79/2021/10/price-tag.png" alt="" />
+                <h1 className="font-extrabold text-[1em] pt-5">Affordable price</h1>
+                <p className="w-64 ">A smartwatch is a wearable computer in the form of a watch.</p>
+                </div>
+
+                <div className="text-left relative text-[1.2em]"></div>
+                <img className="pt-8" src="https://xtratheme.com/elementor/watch-shop/wp-content/uploads/sites/79/2021/10/smartwatch-app.png" alt="" />
+                <h1 className="font-extrabold text-[1em] pt-5">Best Quality and design</h1>
+                <p className="w-80 ">A smartwatch is a wearable computer in the form of a watch.</p>
+            </div>
+
+        </div>
+    </div>
+}
+export default SectionTwo
